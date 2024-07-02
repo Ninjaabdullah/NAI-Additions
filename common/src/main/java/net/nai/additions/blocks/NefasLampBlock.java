4b3bb7f68cc2fecf6bf46dc18979fb1398429dd6
BuildTools <ninjaabdullah.5050@gmail.com>
2024-07-02 14:30:12 +0300

    Added Placement Animations for Virtus & Nefas Lamps

--- common/src/main/java/net/nai/additions/blocks/NefasLampBlock.java
+++ common/src/main/java/net/nai/additions/blocks/NefasLampBlock.java
@@ -5,6 +5,8 @@ import net.minecraft.sounds.SoundEvents;
 import net.minecraft.sounds.SoundSource;
 import net.minecraft.util.Mth;
 import net.minecraft.util.RandomSource;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.BaseEntityBlock;
 import net.minecraft.world.level.block.RenderShape;
@@ -38,6 +40,38 @@ public class NefasLampBlock extends BaseEntityBlock {
     @Override
     public <T extends BlockEntity> BlockEntityTicker<T> getTicker(Level level, BlockState blockState, BlockEntityType<T> blockEntityType) {
         return createTickerHelper(blockEntityType, NAIBlockEntityTypes.NEFAS_LAMP_ENTITY.get(), NefasLampBlockEntity::tick);
+    }
+
+    @Override
+    public void setPlacedBy(Level level, BlockPos blockPos, BlockState blockState, @Nullable LivingEntity livingEntity, ItemStack itemStack) {
+        RandomSource randomSource = RandomSource.create();
+        BlockPos.MutableBlockPos mutableBlockPos = new BlockPos.MutableBlockPos();
+        new Thread(() -> {
+            try {
+                for (int radius = 0; radius <= 32; radius++) {
+                    for (int x = -radius; x <= radius; x++) {
+                        for (int z = -radius; z <= radius; z++) {
+                            if (Mth.abs(x) == radius || Mth.abs(z) == radius) {
+                                for (int amount = 0; amount < 4; ++amount) {
+                                    double particleX = blockPos.getX() + x + randomSource.nextDouble();
+                                    double particleY = blockPos.getY();
+                                    double particleZ = blockPos.getZ() + z + randomSource.nextDouble();
+                                    mutableBlockPos.set(particleX, particleY, particleZ);
+                                    BlockState blockState1 = level.getBlockState(mutableBlockPos);
+                                    if (!blockState1.isCollisionShapeFullBlock(level, mutableBlockPos)) {
+                                        level.addParticle(NAIParticles.NEFAS_PARTICLE.get(), particleX, particleY + 0.5, particleZ, 0.0, 0.075, 0.0);
+                                    }
+                                }
+                            }
+                        }
+                    }
+                    Thread.sleep(120); // 50 milliseconds delay between each radius increment
+                }
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+        }).start();
+        super.setPlacedBy(level, blockPos, blockState, livingEntity, itemStack);
     }
 
     @Override
